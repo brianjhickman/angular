94495f9b21842f308a921bfb94e10eefb7f3064b
Brian Hickman <brian.hickman@walmart.com>
2019-03-20 11:30:55 -0500

    Assignment 2: Databinding

---
+++ basics-assignment-2/src/app/username/username.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html'
+})
+export class UsernameComponent implements OnInit {
+  username = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onResetUsername() {
+    this.username = '';
+  }
+
+}
