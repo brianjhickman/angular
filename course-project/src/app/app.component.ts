48ce195a8de19df00e086d8b4399f7d9044a617c
Brian Hickman <brian.hickman@walmart.com>
2019-04-22 16:42:28 -0500

    course-project - Initial component navigation solution

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  clickValue: string = 'recipes';
+
+  onNavbarClicked(navbarClick: string) {
+    this.clickValue = navbarClick;
+  }
+
 }
--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navbarClicked = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipesClick() {
+    this.navbarClicked.emit('recipes');
+  }
+
+  onShoppingListClick() {
+    this.navbarClicked.emit('shoppingList');
   }
 
 }
