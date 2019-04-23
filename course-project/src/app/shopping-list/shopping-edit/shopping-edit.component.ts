9b0c0a61a2e2b57a518316ecc3c7d4f446dccb22
Brian Hickman <brian.hickman@walmart.com>
2019-04-23 15:54:13 -0500

    course-project - Sect. 6.85 checkpoint

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  newIngredient: Ingredient;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addValues(nameInput, amountInput) {
+    this.newIngredient = new Ingredient(nameInput.value, amountInput.value);
+    this.ingredientAdded.emit(this.newIngredient);
   }
 
 }
